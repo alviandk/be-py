92d45fff7ba257f6a768347fbb0ed5d20ddc674e
alviandk <alviandk@gmail.com>
2021-02-07 23:31:51 +0800

    env debug

--- be_py/settings.py
+++ be_py/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['127.0.0.1'])
 
