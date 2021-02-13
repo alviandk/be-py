3a31851a3237d6962c180f808f9cb2092eab9444
alviandk <alviandk@gmail.com>
2021-02-13 16:29:54 +0700

    learn detail layout

--- course/views.py
+++ course/views.py
@@ -8,5 +8,5 @@ def course_list_view(request):
 
     # context['project_list'] = Project.objects.all()
 
-    # return render(request, 'learn/learn_text_list.html', context)
-    return render(request, 'static_page/under_construction.html', context)
+    return render(request, 'learn/learn_text_list.html', context)
+    # return render(request, 'static_page/under_construction.html', context)
